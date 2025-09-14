a5c81909b5e7e4706492d9490c147b566b526635
Prof. J. L. Gregório <jlgregorio81@hotmail.com>
2025-09-14 10:27:46 -0300

    Changing Exceptions

--- src/main/java/br/com/jlgregorio/MyStore/exceptions/GlobalHandlerExceptions.java
+++ src/main/java/br/com/jlgregorio/MyStore/exceptions/GlobalHandlerExceptions.java
@@ -1,30 +1,49 @@
 package br.com.jlgregorio.MyStore.exceptions;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.handler.ResponseStatusExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.Date;
 
 @ControllerAdvice
 public class GlobalHandlerExceptions {
 
+
     @ExceptionHandler(Exception.class)
-    public final CustomExceptionResponse handleAllExceptions(
-            Exception ex, WebRequest request
+    public final String handleAllExceptions(
+            Exception ex, WebRequest request, Model model
     ){
+        String message;
+        String redirectTo;
+        System.out.println("classe: " + ex.getClass().toString());
+        if(ex instanceof NoResourceFoundException){
+            message = "Página ou arquivo não encontrado!";
+            redirectTo = "error404" ;
+        } else {
+            message = ex.getMessage();
+            redirectTo = "error";
+        }
         CustomExceptionResponse response = new CustomExceptionResponse(new Date(),
-                ex.getMessage(), request.getDescription(true));
-        return response;
+                message, request.getDescription(true));
+        //add the model atribute, a data to show
+        model.addAttribute("error", response);
+        //redirect to error page
+        return redirectTo;
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public final CustomExceptionResponse handleResourceNotFoundException(
-            Exception ex, WebRequest request
+    public final String handleResourceNotFoundException(
+            ResourceNotFoundException ex, WebRequest request, Model model
     ){
         CustomExceptionResponse response = new CustomExceptionResponse( new Date(),
                 "Recurso não encontrado!", request.getDescription(true));
-        return response;
+
+        model.addAttribute("error", response);
+        return "error";
     }
 
 }
